215d2d61bf2543e7aa0b2309a822354c098f8ec8
Apurv Nerlekar <apurvnerlekar@gmail.com>
2015-12-14 19:04:04 -0800

    added more endpoints

---
+++ src/rest/model/Events.scala
@@ -0,0 +1,20 @@
+package rest.model
+
+class Events {
+
+  val mongodbConnection = new MongodbConnection()
+
+  def createEvent(map: Map[String, String]): String = {
+
+    mongodbConnection.insert(map, "createEvents")
+
+    "New Event Created"
+  }
+
+  def deleteEvent(map: Map[String, String]): String = {
+    mongodbConnection.delete(map, "createEvents")
+
+    "Event Deleted"
+  }
+
+}
